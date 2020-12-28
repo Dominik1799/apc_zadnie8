44d14973da8e4683f3ea30b1a71e31c011fab975
Dominik1799 <dom.horvath17@gmail.com>
2020-12-28 23:22:56 +0100

    sadasd

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include <vector>
 #include <string>
 
-struct inputImage {
+struct Image {
     std::string path;
     std::vector<int> buffer;
     size_t width;
@@ -11,12 +11,33 @@ struct inputImage {
     size_t depth;
 };
 
+struct App {
+    int readType;
+    int writeType;
+    Image inputImage;
+    Image outputImage;
+};
+
+App parseInput(int argc, char* argv[]) {
+    App app{};
+    if (argc != 4)
+        std::exit(1);
+
+    if (argv[3][1] == '\0' || argv[3][0] != 'P' || argv[3][2] != '\0' || argv[3][1]-'0' > 6 || argv[3][1]-'0' < 1)
+        exit(1);
+
+    app.writeType = argv[3][1] - '0';
+    app.inputImage.path = argv[1];
+    app.outputImage.path = argv[2];
+    return app;
+}
+
 
 
 
 
 
 int main(int argc, char* argv[]) {
-    std::cout << "Hello, World!" << std::endl;
+    App app = parseInput(argc, argv);
     return 0;
 }
