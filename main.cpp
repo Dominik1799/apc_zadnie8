5f932c30f22684fc2a2c73a3a26827f2e67aba7f
Dominik1799 <dom.horvath17@gmail.com>
2020-12-28 19:44:10 +0100

    sadasd

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
 
-int main() {
+struct inputImage {
+    std::string path;
+    std::vector<int> buffer;
+    size_t width;
+    size_t height;
+    size_t depth;
+};
+
+
+
+
+
+
+int main(int argc, char* argv[]) {
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
